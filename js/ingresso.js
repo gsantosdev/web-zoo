6d4675130db2b7453a5fa4dbd4925a813a77ab57
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2021-07-21 20:10:15 -0300

    entrando dado com enter e limpando campo

--- js/ingresso.js
+++ js/ingresso.js
@@ -9,12 +9,24 @@ function onMount() {
     formataValor(VALOR_INGRESSO);
 
   document.getElementById("valor-total").textContent = formataValor(0);
+
+  document.getElementById("ingressante").addEventListener("keyup", (evento) => {
+    if (evento.key === "Enter") {
+      evento.preventDefault();
+      document.getElementById("adicionar").click();
+     }
+  });
 }
 
-function somar() {}
+function somar() {
+  const campo = document.getElementById("valor-total");
+  const total = Number(campo.textContent.replace(",", ".")) + VALOR_INGRESSO;
+  campo.textContent = total;
+}
 
 function adicionar() {
-  const nome = document.getElementById("ingressante").value.trim();
+  const campo = document.getElementById("ingressante");
+  const nome = campo.value.trim();
 
   if (nome.length < 8) {
     document.getElementById("erro").className = "visivel";
@@ -27,5 +39,8 @@ function adicionar() {
   const lista = document.getElementById("lista-compra");
   lista.appendChild(item);
 
+  somar();
+
+  campo.value = "";
   document.getElementById("erro").className = "invisivel";
 }
